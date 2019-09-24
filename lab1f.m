28786dc99c645f13cf7921f1ad4e8d72c42022ba
megangr06 <megangr1997@yahoo.com>
2019-09-23 23:08:56 -0500

    Lab #1 - Question #5

---
+++ lab1f.m
@@ -0,0 +1,12 @@
+%function [d,t] = lab1f(np,nd)
+%if (nargin < 1), 
+np = 1e6; nd = 2; 
+hp = gcp('nocreate');
+if isempty(hp), hp = parpool(4); end
+aA = randn(np, nd); aB = randn(np,nd);
+dA = distributed(aA); dB = distributed(aB);
+d = zeros(np,1);
+tic;
+dc = sqrt(sum((aA-aB).^2,2));
+d = gather(dc);
+t = toc;
