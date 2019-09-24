3617fb321169ba83932c11339988b3582e3ce5f2
megangr06 <megangr1997@yahoo.com>
2019-09-23 22:17:02 -0500

    Lab #1 - Question #2
    The script used for the tests for Question #2.

---
+++ lab1b.m
@@ -0,0 +1,17 @@
+%function [d,t] = lab1b(np,nd, nw)
+%if (nargin < 1), np = 1e1; nd = 10; nw=1; end
+np = 1e7; nd = 10; nw=8;
+hp = gcp('nocreate');
+if isempty(hp), hp=parpool(nw); end
+A = randn(np, nd); B = randn(np,nd);
+d = zeros(np,1);
+tic;
+parfor i = 1:np
+    for j = 1:nd
+        d(i) = d(i) + (B(i,j)-A(i,j)).^2;
+    end
+    d(i) = sqrt(d(i));
+end
+%t = toc
+toc
+delete(hp);
