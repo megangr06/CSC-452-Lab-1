78bec16b9deb19650518451432ed6fb0ae6729fc
megangr06 <megangr1997@yahoo.com>
2019-09-26 00:49:18 -0500

    lab1f - Question #5

--- lab1f.m
+++ lab1f.m
@@ -2,7 +2,7 @@
 %if (nargin < 1), 
 np = 1e6; nd = 2; 
 hp = gcp('nocreate');
-if isempty(hp), hp = parpool(4); end
+if isempty(hp), hp = parpool(8); end
 aA = randn(np, nd); aB = randn(np,nd);
 dA = distributed(aA); dB = distributed(aB);
 d = zeros(np,1);
